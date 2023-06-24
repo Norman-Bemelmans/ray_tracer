1c1e07965361d1c99e994dd97be5e4e5a3499a7b
Norman Bemelmans <norman_bemelmans@proton.me>
2023-06-24 18:03:42 -0400

    Vector3 header and implementation

---
+++ Vector3.cpp
@@ -0,0 +1,72 @@
+// Vector3 implementation file
+
+#include "Vector3.h"
+#include <cmath>
+
+Vector3::Vector3() : x(0), y(0), z(0) {}
+Vector3::Vector3(double x_init, double y_init, double z_init)
+    : x(x_init), y(y_init), z(z_init) {}
+
+Vector3 Vector3::operator+(const Vector3& other) const
+{
+    return Vector3(x + other.x, y + other.y, z + other.z);
+}
+
+Vector3& Vector3::operator+=(const Vector3& other)
+{
+    x += other.x; y += other.y; z += other.z;
+    return *this;
+}
+
+Vector3 Vector3::operator-(const Vector3& other) const
+{
+    return Vector3(x - other.x, y - other.y, z - other.z);
+}
+
+Vector3& Vector3::operator-=(const Vector3& other)
+{
+    x -= other.x; y -= other.y; z -= other.z;
+    return *this;
+}
+
+Vector3 Vector3::operator*(const double scalar) const
+{
+    return Vector3(x * scalar, y * scalar, z * scalar);
+}
+
+Vector3& Vector3::operator*=(const double scalar)
+{
+    x *= scalar; y *= scalar; z *= scalar;
+    return *this;
+}
+
+Vector3 Vector3::operator/(const double scalar) const
+{
+    return Vector3(x / scalar, y / scalar, z / scalar);
+}
+
+Vector3& Vector3::operator/=(const double scalar)
+{
+    x /= scalar; y /= scalar; z /= scalar;
+    return *this;
+}
+
+Vector3 Vector3::operator-()
+{
+    return Vector3(-x, -y, -z);
+}
+
+double Vector3::length_squared() const
+[
+    return x*x + y*y + z*z;
+]
+
+double Vector3::length() const
+{
+    return std::sqrt(length_squared());
+}
+
+Vector3 Vector3::normalize()
+{
+    
+}
--- Vector3.h
+++ Vector3.h
@@ -14,21 +14,26 @@ public:
     Vector3();
     Vector3( double x_init, double y_init, double z_init);
 
-    Vector3 operator+(const Vector3& other);
-    Vector3 operator+=(const Vector3& other);
-    Vector3 operator-(const Vector3& other);
-    Vector3 operator-=(const Vector3& other);
+    Vector3 operator+(const Vector3& other) const;
+    Vector3& operator+=(const Vector3& other);
+    Vector3 operator-(const Vector3& other) const;
+    Vector3& operator-=(const Vector3& other);
 
     // multiplication of vector v and scalar s must be written v*s:
-    Vector3 operator*(const double scalar);
-    Vector3 operator*=(const double scalar);
-    Vector3 operator/(const double scalar);
-    Vector3 operator/=(const double scalar);
+    Vector3 operator*(const double scalar) const;
+    Vector3& operator*=(const double scalar);
+    Vector3 operator/(const double scalar) const;
+    Vector3& operator/=(const double scalar);
 
     //unary '-' changes the sign of a vector:
     Vector3 operator-();
+
+    double length_squared() const;
+    double length() const;
 
     Vector3 normalize();
     double dot_prod(Vector3 other);
     Vector3 cross_prod(Vector3 other);
 };
+
+#endif
