bb36ac64307ceac589e9c3a4397c7df389d7c689
Norman Bemelmans <nlbemelmans@gmail.com>
2023-07-20 10:50:32 -0400

    Testing new SSH key...

---
+++ src/camera/Camera.hpp
@@ -0,0 +1,27 @@
+// Camera class declarations.
+
+#ifndef CAMERA_HPP
+#define CAMERA_HPP
+
+#include "Vector3.hpp"
+#include "Point3.hpp"
+
+namespace RT {
+
+    class Camera {
+    private:
+        // eye-point is  the camera origin.
+        Point3 eye_point;
+        // view_dir is the direction the camera is looking.
+        Vector3 view_dir;
+        // up is a vector that points up relative to the camera.
+        Vector3 up;
+
+        // focal_length is the distance between the eye_point and
+        // the image plane.
+        float focal_length;
+
+        // Coordinates for top, bottom, left, and right of image plane.
+        float t, b, l, r;
+    };
+}
