2c080859ebdc47e15b3c0634ddd6f230f5c8ef42
norman <norman_bemelmans@proton.me>
2023-08-24 14:03:03 -0400

    updated Hit_Record description.

--- src/utility/Hit_Record.hpp
+++ src/utility/Hit_Record.hpp
@@ -1,4 +1,3 @@
 // Hit_Record class declarations
-// Hit_Record is the glue that holds the shading architecture together. It 
-// allows for information transfer between the geometry and shading 
+// Hit_Record allows for information transfer between the geometry and shading 
 // subsystems of the tracer. 
