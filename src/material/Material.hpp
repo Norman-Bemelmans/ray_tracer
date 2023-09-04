e2f548dff00abc9af53fca2eec8bf52419f4fd99
norman <norman_bemelmans@proton.me>
2023-09-04 13:20:32 -0400

    Material Update.

--- src/material/Material.hpp
+++ src/material/Material.hpp
@@ -1,2 +1,15 @@
 // Material (abstract class) declaration
+// Each derived class must provide a Shade() method which computes 
+// the radiance at a given pixel.
 
+#ifndef MATERIAL_HPP
+#define MATERIAL_HPP
+
+#include <vector>
+
+#include "Light.hpp" 
+
+namespace RT {
+
+    
+}
