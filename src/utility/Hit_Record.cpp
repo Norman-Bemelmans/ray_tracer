edd975cde4defd6104800ade17677945e3a0a5e2
norman <norman_bemelmans@proton.me>
2023-09-04 11:46:28 -0400

    Implemented Hit_Record class.

---
+++ src/utility/Hit_Record.cpp
@@ -0,0 +1,11 @@
+// Hit_Record class implementation
+
+#include "Hit_Record.hpp"
+#include "Normal.hpp"
+#include "Point3.hpp"
+#include "Material"
+
+namespace RT {
+
+    Hit_Record::Hit_Record() hp(0.0, 0.0, 0.0), sn(0.0, 0.0, 0.0), mat = NULLPTR {}
+}
--- src/utility/Hit_Record.hpp
+++ src/utility/Hit_Record.hpp
@@ -1,3 +1,24 @@
 // Hit_Record class declarations
 // Hit_Record allows for information transfer between the geometry and shading 
 // subsystems of the tracer. 
+
+#ifndef HIT_RECORD_CPP
+#define HIT_RECORD_CPP
+
+#include <memory>
+
+#include "Normal.hpp"
+#include "Point3.hpp"
+#include "Material.hpp"
+
+namespace RT {
+
+    class Hit_Record {
+    public:
+        Hit_Record();
+    private:
+        Point3 hp;
+        Normal sn;
+        std::unique_ptr<Material> mat;
+    }
+}
