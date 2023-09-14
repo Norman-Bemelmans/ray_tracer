d36a3fcf0443eb23ef9f956e759ade1db82667f8
Norman Bemelmans <norman_bemelmans@proton.me>
2023-09-13 22:44:14 -0400

    Hit Record class unit test added.

---
+++ test/utility/Hit_Record_test.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+sudo g++ -I/home/norman/code/ray_tracer/src/geometry \
+/home/norman/code/ray_tracer/src/geometry/Point3.cpp \
+/home/norman/code/ray_tracer/src/geometry/Normal.cpp \
+/home/norman/code/ray_tracer/src/geometry/Vector3.cpp \
+-I/home/norman/code/ray_tracer/src/utility \
+/home/norman/code/ray_tracer/src/utility/Hit_Record.cpp \
+/home/norman/code/ray_tracer/src/utility/Hit_Record.test.cpp \
+-o Hit_Record_test
+./Hit_Record_test
