7669829d10cd88562a5788aa63d6774fcf9996dd
Norman Bemelmans <norman_bemelmans@proton.me>
2023-09-21 10:36:09 -0400

    Matte class updated so it works.

--- src/material/Matte.cpp
+++ src/material/Matte.cpp
@@ -1,6 +1,7 @@
 // Matte (derived from Material) class implementation
 
 #include <vector>
+#include <iostream>
 
 #include "Matte.hpp"
 #include "RGB_Color.hpp"
@@ -11,7 +12,9 @@ namespace RT{
     Matte::Matte() : ka(1.0), kd(1.0) , cd(0.5, 0.5, 0.5) {}
     Matte::Matte(float ka, float kd, RGB_Color cd) : ka(ka), kd(kd), cd(cd) {}
 
-    // For now, I am ignoring global illumination. Later versions will use ka.
+    // For now, I am ignoring global illumination and the directional aspect
+    // of the shading calculation. These will need to wait for more classes 
+    // in order to be implemented.
     RGB_Color Matte::shade(const Hit_Record& hr, const std::vector<Light*>& lv)
     {
         // return pixel radiance value:
