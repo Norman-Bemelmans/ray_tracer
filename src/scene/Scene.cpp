afc5ffa31d8205608236aa50c59238e9ea95a585
Norman Bemelmans <norman_bemelmans@proton.me>
2023-12-17 22:06:32 -0500

    Added progree update messages.

--- src/scene/Scene.cpp
+++ src/scene/Scene.cpp
@@ -13,18 +13,24 @@
 #include "HitRecord.hpp"
 #include "Ray.hpp"
 
+#include <iostream>
+
 namespace RT {
 // A future iteration of the tracer will read data in from a file (probably XML). For now,
 // I will hard-code simple values just to get the tracer up and running.
 void Scene::init() {
+    std::cout << "Initializing lights..." << "\n";
     // Add light to scene:
     PointLight light(RGBColor(1.0, 1.0,  1.0), Point3(0.0, -1.0, 0.0));
     lightContainer.push_back(light);
+    std::cout << "Lights loaded." << "\n";
 
     // Add shape to scene:
+    std::cout << "Initializing scene geometry..." << "\n";
     Material* mat = new Matte(RGBColor(1.0, 1.0, 1.0), RGBColor(0.0, 0.5, 0.0));
     Sphere ball(Point3(0.0, 0.0, 5.0), 1.0, mat);
     shapeContainer.push_back(ball);
+    std::cout << "Scene geometry loaded." << "\n";
 
     // Add camera to scene:
     pCamera = new Pinhole(Point3(0.0, 0.0, 0.0), Point3(0.0, 0.0, 1.0), 5.0, 300, 400,
@@ -34,6 +40,7 @@ void Scene::init() {
 }
 
 void Scene::renderScene() {
+    std::cout << "Rendering scene..." << "\n";
     // For each pixel in the viewport:
     for (int i = 0; i < pCamera->hres; < ++i;) {
         for (int j = 0; j < pCamera->vres < ++j;) {
@@ -58,5 +65,6 @@ void Scene::renderScene() {
             pFilm->addToBitmap(radiance);
         }
     }
+    std::cout << "Scene successfully rendered." << "\n";
 }
 } // namespace RT
